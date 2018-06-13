c3fa7d54d385c4a810424d083580d6ffe194e0a0
komota <komotatit@gmail.com>
2018-06-13 21:52:03 +0900

    [update - BrainfuckInterpreter.py] 完成．helloworld で動作確認済み

--- brainfuck/BrainfuckInterpreter.py
+++ brainfuck/BrainfuckInterpreter.py
@@ -8,19 +8,22 @@ import config
 class Interpreter:
     
     def __init__(self, target):
-        self.target = target
-        self.tape = [0 for _ in range(config.TAPE_LENGTH)]
-        self.head = 0
+        self.target   = target
+        self.code     = ""
+        self.tape     = [0 for _ in range(config.TAPE_LENGTH)]
+        self.head     = 0
+        self.lbMarker = []
 
     def run(self):
         # return self.target
 
-        code = self.target
+        self.code = self.target
         c    = "?"
 
-        while len(code) > 0 and c != "#":
-            c    = code[0]
-            code = code[1:] 
+        while len(self.code) > 0 and c != "#":
+            # self.showTape()
+            c    = self.code[0]
+            self.code = self.code[1:] 
             if   c == ">":
                 self.LT()
             elif c == "<":
@@ -41,19 +44,50 @@ class Interpreter:
                 pass
 
     def LT(self):
-        print("LT, ", end="")
+        # print("LT, ", end="")
+        self.head = min(self.head+1, config.TAPE_LENGTH)
     def GT(self):
-        print("GT, ", end="")
+        # print("GT, ", end="")
+        self.head = max(self.head-1, 0)
     def PL(self):
-        print("PL, ", end="")
+        # print("PL, ", end="")
+        current = self.tape[self.head]
+        self.tape[self.head] = min(current+1, config.TAPE_MAX)
     def MI(self):
-        print("MI, ", end="")
+        # print("MI, ", end="")
+        current = self.tape[self.head]
+        self.tape[self.head] = max(current-1, config.TAPE_MIN)
     def LB(self):
-        print("LB, ", end="")
+        # print("LB, ", end="")
+        if self.tape[self.head] > 0:
+            tmp = len(self.target)-len(self.code)-1
+            self.lbMarker.append(tmp)
+        else:
+            while self.code[0] != "]":
+                self.code = self.code[1:]
+            self.code = self.code[1:]
     def RB(self):
-        print("RB, ", end="")
+        # print("RB, ", end="")
+        if self.tape[self.head] == 0:
+            pass
+        else:
+            self.code = self.target[self.lbMarker[-1]:]
+            self.lbMarker = self.lbMarker[:-1]
     def DO(self):
-        print("DO, ", end="")
+        # print("DO, ", end="")
+        # print(chr(ord("A")+self.tape[self.head]), end="")
+        print(chr(self.tape[self.head]), end="")
+        # print(self.tape[self.head], end=" ")
+
+    def showTape(self):
+        print()
+        print(self.code)
+        br = ""
+        for i in range(self.head):
+            br += "   "
+        print(br + "v")
+        for i in range(10):
+            print("{0:02d}".format(self.tape[i]), end="|")
 
 if __name__ == "__main__":
 
--- brainfuck/config.py
+++ brainfuck/config.py
@@ -1,3 +1,5 @@
 #coding : utf-8
 
 TAPE_LENGTH = 500
+TAPE_MAX    = 127
+TAPE_MIN    = 0
